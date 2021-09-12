36331fb3bf3cb24f1694f91e2aa1583eb69a8638
mattak <mattak.me@gmail.com>
2021-09-12 10:55:32 +0900

    :seedling: create GameObjectActiveRenderer

---
+++ Assets/UnityHooks/Scripts/Renderer/GameObjectActiveRenderer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UnityHooks.Renderer
+{
+    public class GameObjectActiveRenderer : MonoBehaviour
+    {
+        public void Render(bool active)
+        {
+            this.gameObject.SetActive(active);
+        }
+    }
+}
